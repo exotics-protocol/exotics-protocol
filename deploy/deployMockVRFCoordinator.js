883ff5bc6c3da718e5a66a60b148c5c09271d41b
jummy123 <jummy123@users.noreply.github.com>
2022-06-27 22:36:28 +0100

    Add networks to mock vrf deploy

--- deploy/deployMockVRFCoordinator.js
+++ deploy/deployMockVRFCoordinator.js
@@ -1,11 +1,22 @@
 module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
   const {deploy, execute} = deployments;
   const {deployer} = await getNamedAccounts();
-  await deploy('MockVRFCoordinator', {
-    contract: 'MockVRFCoordinator',
-    from: deployer,
-    log: true,
-  });
+  const chainId = await getChainId();
+
+  // Avalanche mainnet uses chainlink
+  if (chainId == 43114) {
+  }
+  // avalanche fuji uses chainlink
+  else if (chainId == 43113) {
+  }
+  // hardhat uses mock usdc
+  else if (chainId == 31337) {
+    await deploy('MockVRFCoordinator', {
+      contract: 'MockVRFCoordinator',
+      from: deployer,
+      log: true,
+    });
+  }
 };
 
 module.exports.tags = ["MockVRFCoordinator"];
