3fb0baba8a29c679eaf1357e84b6f4061d63eb97
jummy123 <jummy123@users.noreply.github.com>
2022-06-27 23:09:52 +0100

    Fuji deploy

--- deploy/DeployExotic.js
+++ deploy/DeployExotic.js
@@ -3,25 +3,22 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
   const {deployer} = await getNamedAccounts();
   const chainId = await getChainId();
 
-  // Avalanche mainnet uses chainlink
   let vrfAddress, vrfSubscription;
   if (chainId == 43114) {
+    // Avalanche mainnet uses chainlink
 
   }
-  // avalanche fuji uses chainlink
   else if (chainId == 43113) {
+    // avalanche fuji uses chainlink
     vrfAddress = '0x2eD832Ba664535e5886b75D64C46EB9a228C2610';
     // TODO: key hash
     vrfSubscription = 167;
-
   }
-  // hardhat uses mock vrf
   else if (chainId == 31337) {
+    // hardhat uses mock vrf
     vrfAddress = (await deployments.get('MockVRFCoordinator')).address;
     vrfSubscription = 0;
   }
-
-
 
   await deploy('Exotic', {
     contract: 'Exotic',
