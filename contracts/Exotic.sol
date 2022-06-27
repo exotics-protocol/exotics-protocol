2cef7bcc53582327b64bc9e94092a7bdd61b746c
jummy123 <jummy123@users.noreply.github.com>
2022-06-27 22:35:34 +0100

    Remove all but `win` bet

--- contracts/Exotic.sol
+++ contracts/Exotic.sol
@@ -27,6 +27,7 @@ contract Exotic is VRFConsumerBaseV2 {
     uint256 public immutable start;
 
     struct Bet {
+        uint256 raceId;
         uint256 amount;
         address account;
         uint256[] place;
@@ -37,13 +38,15 @@ contract Exotic is VRFConsumerBaseV2 {
         uint256 fee;  // House take in bps
         uint256 totalWagered;
         uint256 paid;
-        uint256[] result;
-        Bet[] bets;
-        uint256[6][6] weights;
+        uint256 result;
+        uint256[6][3] weights;
     }
 
     /// @notice The state for each race.
     mapping(uint256 => Race) public race;
+    /// @notice mapping of address to list of bets.
+    mapping(address => Bet[]) public bet;
+    /// @notice used internally to map races to VRF requests.
     mapping(uint256 => uint256) private requestIdRace;
 
     constructor(uint64 subscriptionId, address _vrfCoordinator) VRFConsumerBaseV2(_vrfCoordinator) {
@@ -52,111 +55,79 @@ contract Exotic is VRFConsumerBaseV2 {
 		s_subscriptionId = subscriptionId;
     }
 
-    /// @notice Place a win bet.
-    function win(
+	/// @notice The time of the next race to take place.
+	function nextRaceId() external view returns (uint256) {
+		return block.timestamp + (frequency - (block.timestamp % frequency));
+	}
+
+    /// @notice Get the current odds for a prediction.
+    function odds(uint256 raceId, uint256[] memory result) public view returns (uint256) {
+        Race memory _race = race[raceId];
+        require(result.length == 1, "Only win bet currently supported");
+        uint256 total;
+        uint256 i;
+        for (i = 0; i < 6; i++) {
+            total += _race.weights[0][i];
+        }
+        if (total == 0) return total;
+        return (_race.weights[0][result[0]] * 1e10) / total;
+    }
+
+    /// @notice Place a bet.
+    function placeBet(
         uint256 raceId,
-        uint256 first
+        uint256[] calldata prediction
     ) external payable returns (uint256 betId) {
         validateRaceID(raceId);
+        require(prediction.length == 1, "Only win bet currently supported");
 
-		console.log('raceId', raceId);
-
-        uint256[] memory place;
         // Create the bet.
-        Bet memory bet = Bet(msg.value, msg.sender, place, false);
-        // Get a race and push the new bet to it.
-        Race storage _race = race[raceId];
-        _race.bets.push(bet);
-        // Add the bet.
-		console.log('bets len', _race.bets.length);
-        _race.bets[_race.bets.length-1].place.push(first);
+        Bet memory _bet;
+        _bet.raceId = raceId;
+        _bet.amount = msg.value;
+        _bet.account = msg.sender;
+        bet[msg.sender].push(_bet);
+        bet[msg.sender][bet[msg.sender].length - 1].place.push(prediction[0]);
 
-        // Update the weight table.
-        _race.weights[0][first] += msg.value;
-		console.log('updated race weights [0][first]', _race.weights[0][first]);
+        // Update the race.
+        Race storage _race = race[raceId];
+        _race.weights[0][prediction[0]] += msg.value;
         _race.totalWagered += msg.value;
+
+        // Internal accounting.
         balance += msg.value;
-        betId = _race.bets.length;
+        return bet[msg.sender].length - 1;
     }
 
-    /// @notice Place a forecast bet.
-    function forecast(
-        uint256 raceId,
-        uint256 amount,
-        uint256 first,
-        uint256 second
-    ) external payable {
-        validateRaceID(raceId);
-        raceId; amount; first; second;
-    }
-
-    /// @notice place a tricast bet.
-    function tricast(
-        uint256 raceId,
-        uint256 amount,
-        uint256 first,
-        uint256 second,
-        uint256 third
-    ) external payable {
-        validateRaceID(raceId);
-        raceId; amount; first; second; third;
-    }
-
-    function payout(
-        uint256 raceId,
-        uint256 betId
-    ) external {
-        Race storage _race = race[raceId];
-        require(_race.result.length != 0, "Race not finished");
-        require(_race.bets[betId].account == msg.sender, "Not bet owner");
-        Bet storage _bet = _race.bets[betId];
+    /// @notice Cash out a winning bet.
+    function payout(uint256 betId) external {
+        Bet storage _bet = bet[msg.sender][betId];
+        Race storage _race = race[_bet.raceId];
+        require(_race.result != 0, "Race not finished");
         require(!_bet.paid, "Bet already paid");
-        // We need the result.
-        uint256[] memory result = raceResult(raceId);
-        // Need to check if bet was a winning bet.
+
+        uint256[6] memory result = raceResult(_bet.raceId);
         uint256 i;
-        bool winner = true;
         for (i = 0; i < _bet.place.length; i++) {
             if (_bet.place[i] != result[i]) {
-                winner = false;
+                return;
             }
         }
-        if (!winner) {
-            return;
-        }
-        // We need to calculate the winnings.
-        uint256 _odds = odds(raceId, _bet.place);
-        uint256 _payout = _odds * _bet.amount;
+        uint256 _odds = odds(_bet.raceId, _bet.place);
+        uint256 _payout = (_bet.amount * 1e10) / _odds;
         _bet.paid = true;
         _race.paid += _payout;
         balance -= _payout;
         payable(msg.sender).transfer(_payout);
     }
 
-	// The value of a bet.
-	function betValue(uint256 raceId, uint256 betId) external view returns (uint256) {
-
-	}
-
-	// @title the time of the next race.
-	function nextRaceId() external view returns (uint256) {
-		return block.timestamp + (frequency - (block.timestamp % frequency));
-	}
-
-	function results(uint256 raceId) public view returns (uint256[] memory) {
-        Race memory _race = race[raceId];
-		console.log('race `results` set in memory', _race.result.length);
-		return _race.result;
-	}
-
+    /// @notice End the race and request result from VRF.
     function endRace(uint256 raceId) external {
         validateRaceID(raceId);
-		console.log('timestamp of block at endRace', block.timestamp);
-		console.log('raceId at endRace', raceId);
         require(block.timestamp > raceId + frequency, "Race not finished");
         require(requestIdRace[raceId] == 0, "Result already requested");
         Race storage _race = race[raceId];
-        require(_race.result.length == 0, "Race result already fulfilled");
+        require(_race.result == 0, "Race result already fulfilled");
 	 	uint256 s_requestId = COORDINATOR.requestRandomWords(
 		  keyHash,
 		  s_subscriptionId,
@@ -164,114 +135,51 @@ contract Exotic is VRFConsumerBaseV2 {
 		  callbackGasLimit,
 		  numWords
 		);
-		console.log('race ended', s_requestId, raceId);
         requestIdRace[s_requestId] = raceId;
-		console.log('race id saved?', requestIdRace[s_requestId]);
     }
 
+    /// @notice VRF callback function.
     function fulfillRandomWords(
         uint256 requestId,
         uint256[] memory randomWords
     ) internal override {
-		console.log('fulfillRandomWords called', randomWords[0], requestId);
         uint256 raceId = requestIdRace[requestId];
-		console.log('raceId ', raceId);
         Race storage _race = race[raceId];
-        require(_race.result.length == 0, "Randomness already fulfilled");
-        _race.result.push(randomWords[0]);
-        require(_race.result.length != 0, "Randomness already fulfilled");
-		console.log('is the result getting set?', _race.result[0]);
+        require(_race.result == 0, "Randomness already fulfilled");
+        _race.result = randomWords[0];
     }
 
+    /// @notice Validate a `raceId` is valid to make a bet on.
     function validateRaceID(uint256 raceId) internal view {
         // Need to validate race length isn't finished
         require(raceId % frequency == 0, "Invalid race ID");
         require(raceId >= start, "Living in the past bro");
-        require(race[raceId].result.length == 0, "Race finished");
+        require(race[raceId].result == 0, "Race finished");
         require(requestIdRace[raceId] == 0, "Race finising");
     }
 
-    function odds(uint256 raceId, uint256[] memory result) public view returns (uint256) {
+    /// @notice Return the results for a race.
+    function raceResult(uint256 raceId) public view returns (uint256[6] memory) {
         Race memory _race = race[raceId];
-        require(result.length <= 3, "Only tricast supported");
-        uint256 notSelected = _race.weights.length - result.length;
-		console.log('notSelected ', notSelected);
-        uint256 placeMultiplier = factorial(notSelected);
-		console.log('placeMultiplier', placeMultiplier);
-		uint256 wildMultiplier = factorial(notSelected > 0 ? notSelected - 1 : 0);
-		console.log('wildMultiplier', wildMultiplier);
+        require(_race.result != 0, "Race is not finished");
+        uint256[6] memory result;
 
-		uint256 total;
-        uint256 i; uint256 j;
-		for (i = 0; i < _race.weights.length; i++) {
-			//console.log('start of iter', total);
-            if (i < result.length) {
-                total += placeMultiplier * _race.weights[i][result[i]];
-            } else if (i >= result.length) {
-                for (j = 0; j < _race.weights[i].length; j++) {
-                    if (!contains(result, j)) {
-                        total += wildMultiplier * _race.weights[i][j];
-                    }
-                }
-            }
-		}
-
-        uint256 all;
-        uint256 allMultiplier = factorial(_race.weights.length - 1);
-		for (i = 0; i < _race.weights.length; i++) {
-			//console.log('start of iter in all', all);
-            for (j = 0; j < _race.weights[i].length; j++) {
-                all += allMultiplier * _race.weights[i][j];
-            }
-        }
-		console.log('total * 1e10 / all', (total * 1e10) / all);
-        return (total * 1e10) / all;
-    }
-
-    function raceResult(uint256 raceId) public view returns (uint256[] memory result) {
-        Race memory _race = race[raceId];
+        uint256 total;
         uint256 i;
+        for (i = 0; i < 6; i++) {
+            total += _race.weights[0][i];
+        }
+        uint256 number = _race.result % total;
+
         uint256 j;
-        uint256[] memory placeOdds;
-        uint256 sumPlaceOdds;
-        uint256[] memory bet;
-        for (i = 0; i < 3; i++) {
-            delete placeOdds;
-            sumPlaceOdds = 0;
-            uint256[6] memory weights = _race.weights[i];
-            for (j = 0; j <= weights.length; j++) {
-                delete bet;
-                bet[0] = j;
-                if (!contains(result, j)) {
-                    uint256 _odd = odds(raceId, bet);
-                    placeOdds[i] = _odd;
-                    sumPlaceOdds += _odd;
-                } else {
-                    placeOdds[i] = 0;
-                }
-            }
-
-        }
-    }
-
-    function factorial(uint256 x) internal pure returns (uint256) {
-        if (x == 0) {
-		  return 1;
-		}
-		else {
-		  return x*factorial(x-1);
-		}
-    }
-
-    /// @dev this will only be used where `bet` contains at most 3 items
-    /// so is probably optimal in that range. Don't use it for larger arrays.
-    function contains(uint256[] memory bet, uint256 contestant) internal pure returns (bool) {
-        uint i;
-        for (i = 0; i < bet.length; i++) {
-            if (bet[i] == contestant) {
-                return true;
+        for (j = 0; j < 6; j++) {
+            if (_race.weights[0][j] > number) {
+                result[0] = j;
+            } else {
+                number -= _race.weights[0][j];
             }
         }
-        return false;
+        return result;
     }
+
 }
--- test/exotic-mvp-test.ts
+++ test/exotic-mvp-test.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import { deployments, ethers, network } from "hardhat";
 
-describe("Exotics MVP test case", function () {
+describe.only("Exotics MVP test case", function () {
   before(async function () {
     this.signers = await ethers.getSigners();
   });
@@ -12,41 +12,98 @@ describe("Exotics MVP test case", function () {
 	this.vrf = await ethers.getContract('MockVRFCoordinator');
   });
 
-  it("should show the time of the next race", async function () {
-	const nextRace = await this.exotic.nextRaceId();
-   	expect(nextRace).is.gt(0);
+  it("should take win bets and give correct odds", async function () {
+    const nextRace = await this.exotic.nextRaceId();
+
+    let odds = await this.exotic.odds(nextRace, [0]);
+    expect(odds).to.eq(0);
+
+    await this.exotic.placeBet(nextRace, [0], {value: ethers.utils.parseEther("1")});
+    odds = await this.exotic.odds(nextRace, [0]);
+    expect(odds).to.eq(1*10**10);
+
+    await this.exotic.placeBet(nextRace, [1], {value: ethers.utils.parseEther("1")});
+    odds = await this.exotic.odds(nextRace, [0]);
+    expect(odds).to.eq(5*10**9);
+    odds = await this.exotic.odds(nextRace, [1]);
+    expect(odds).to.eq(5*10**9);
+
+    await this.exotic.placeBet(nextRace, [1], {value: ethers.utils.parseEther("1")});
+    await this.exotic.placeBet(nextRace, [2], {value: ethers.utils.parseEther("1")});
+    odds = await this.exotic.odds(nextRace, [1]);
+    expect(odds).to.eq(5*10**9);
+    odds = await this.exotic.odds(nextRace, [0]);
+    expect(odds).to.eq(25*10**8);
+    odds = await this.exotic.odds(nextRace, [2]);
+    expect(odds).to.eq(25*10**8);
   });
 
-  it("should allow a win bet", async function () {
-	const nextRace = await this.exotic.nextRaceId();
-	await this.exotic.win(nextRace, 0);
-  });
-
-  it("should return the odds for a bet", async function () {
-	const nextRace = await this.exotic.nextRaceId();
-	await this.exotic.win(nextRace, 0, {value: ethers.utils.parseEther('1')});
-	const odds = await this.exotic.odds(nextRace, [0]);
-	console.log('odds ', odds);
-	expect(odds).to.equal(1*10**10);
-	await this.exotic.win(nextRace, 1, {value: ethers.utils.parseEther('1')});
-	expect(await this.exotic.odds(nextRace, [0])).to.equal(5*10**9);  // 5*10**9
-	expect(await this.exotic.odds(nextRace, [1])).to.equal(5*10**9);  // 5*10**9
-	expect(await this.exotic.odds(nextRace, [2])).to.equal(0);
-  });
-
-  it("simple end to end with payout", async function () {
-	const nextRace = await this.exotic.nextRaceId();
-	await this.exotic.win(nextRace, 0, {value: ethers.utils.parseEther('1')});
-	await this.exotic.win(nextRace, 1, {value: ethers.utils.parseEther('1')});
+  it("A one bet race should always win", async function () {
+    const nextRace = await this.exotic.nextRaceId();
+    await this.exotic.placeBet(nextRace, [0], {value: ethers.utils.parseEther("1")});
 	await network.provider.send("evm_increaseTime", [1199])  // Maximum time possible for race to end as we don't fully control starttime.
 	await this.exotic.endRace(nextRace);
 	await this.vrf.fulfill();
-	console.log('result is ', await this.exotic.results(nextRace));
-	let i = 0;
-	while (i < 100) {
-		console.log("TEST THE PAYOUT FUNCITON");
-		i++;
-	}
-	await this.exotic.payout(nextRace, 1);
+    const balanceBefore = await this.signers[0].getBalance();
+    await this.exotic.payout(0);
+    expect(
+      await this.signers[0].getBalance()
+    ).to.be.closeTo(
+      balanceBefore.add(ethers.utils.parseEther('1')),
+      ethers.utils.parseEther('0.001')
+    );
   });
+
+  it("should return the odds for a win bet", async function () {
+	const nextRace = await this.exotic.nextRaceId();
+	await this.exotic.placeBet(nextRace, [0], {value: ethers.utils.parseEther('1')});
+	const odds = await this.exotic.odds(nextRace, [0]);
+	expect(odds).to.equal(1*10**10);
+	await this.exotic.placeBet(nextRace, [1], {value: ethers.utils.parseEther('1')});
+	expect(await this.exotic.odds(nextRace, [0])).to.equal(5*10**9);
+	expect(await this.exotic.odds(nextRace, [1])).to.equal(5*10**9);
+	expect(await this.exotic.odds(nextRace, [2])).to.equal(0);
+
+	await this.exotic.placeBet(nextRace, [2], {value: ethers.utils.parseEther('2')});
+
+	expect(await this.exotic.odds(nextRace, [0])).to.equal(25*10**8);
+	expect(await this.exotic.odds(nextRace, [1])).to.equal(25*10**8);
+	expect(await this.exotic.odds(nextRace, [2])).to.equal(5*10**9);
+  });
+
+  it("should payout winner and not loser", async function () {
+	const nextRace = await this.exotic.nextRaceId();
+	await this.exotic.placeBet(nextRace, [0], {value: ethers.utils.parseEther('1')});
+	await this.exotic.connect(this.signers[1]).placeBet(nextRace, [1], {value: ethers.utils.parseEther('1')});
+	await network.provider.send("evm_increaseTime", [1199])  // Maximum time possible for race to end as we don't fully control starttime.
+	await this.exotic.endRace(nextRace);
+	await this.vrf.fulfill();
+    const result = await this.exotic.raceResult(nextRace);
+    let winner, loser;
+    if (result[0] == 0) {
+        winner = this.signers[0];
+        loser = this.signers[1];
+    } else {
+        loser = this.signers[0];
+        winner = this.signers[1];
+    }
+    const loserBefore = await loser.getBalance();
+    const winnerBefore = await winner.getBalance();
+    await this.exotic.connect(winner).payout(0);
+    await this.exotic.connect(loser).payout(0);
+    expect(
+      await winner.getBalance()
+    ).to.be.closeTo(
+      winnerBefore.add(ethers.utils.parseEther('2')),
+      ethers.utils.parseEther('0.001')
+    );
+    expect(
+      await loser.getBalance()
+    ).to.be.closeTo(
+      loserBefore,
+      ethers.utils.parseEther('0.001')
+    );
+
+  });
+
 });
